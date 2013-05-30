a9d78300c030824d3c01d69582d5e8482908293b
Dmitry Groshev <groshev@selectel.ru>
2013-05-31 01:10:04 +0400

    fix project.clj, thnx @alwx

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject treegen "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "An experiment with CFG and core.logic"
+  :url "https://github.com/si14/treegen"
+  :license {:name "BSD"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.logic "0.8.3"]
                  [fipp "0.3.0-SNAPSHOT"]]
